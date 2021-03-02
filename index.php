d9b2b460b470f08f386e01f8dd54b1b01d51351e
Ritika-Nunia <63947096+Ritika-Nunia@users.noreply.github.com>
2021-03-02 22:02:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="portfolio.css">
+<title>Ritika Portfolio</title>
+</head>
+<body>
+<!--------------------Navigation Bar-------------------------------->
+<section id="Nav_bar">
+<div class="top_navbar">
+<ul class="list">
+<li class="item"><a class="link" href="#pro">Profile</a></li>
+<li class="item"><a class="link" href="#proj">Projects</a></li>
+<li class="item"><a class="link" href="#skill">Skills</a></li>
+<li class="item"><a class="link" href="#personalskills">Personal Skills</a></li>
+<li class="item"><a class="link" href="#ExtracurricularActivities">Extracurricular Activities</a></li>
+</ul>
+</div>
+</section>
+
+<!-----------------Vertical Container----------------->
+<div class="container">
+<div class="box1">
+<img class="pic" src="ritika.jpg" alt="profile pic">
+<p class="RitikaNunia">RITIKA NUNIA</p>
+<p class="Education">EDUCATION</p>
+<div class="box2">
+<p class="Btech">B.tech(Computer Science & Engineering)<br>
+    (2016-2020)
+<p class="college">College of Engineering Roorkee<br>
+    (Uttarakhand Technical University)</p>
+<p class="Btech">Intermediate</p>
+<p class="college">Piramal Girls Sr. Sec School, Bagar<br>
+    Jhunjhunu, Rajasthan</p>
+<p class="Btech">High School</p>
+<p class="college">Piramal Girls Sr. Sec School, Bagar<br>
+    Jhunjhunu, Rajasthan</p>
+<p class="Education">CONTACT</p>
+<div class="box2"></div>
+<a class="fa fa-mobile-phone phone-color" ></a><p class="phone">+91-8890185186</p>
+<a class="fa fa-at gmail-color" ></a><p class="gmail">ritikanunia2001@gmail.com</p>
+<a class="fa fa-github gmail-color" href="https://github.com/Ritika-Nunia"></a><a class="github">Ritika-Nunia</a>
+<a class="fa fa-instagram gmail-color" href="https://www.instagram.com/nuniaritika/"></a><a class="instagram">nuniaritika</a>
+<a class="fa fa-facebook gmail-color" href="https://www.facebook.com/ritika.nunia.1"></a><a class="facebook">Ritika Nunia</a>
+</div>
+</div>
+</div>
+
+<!----------------------Profile---------------->
+
+<div class="profile">
+<p class="ab"></p>
+<p id="pro">PROFILE</p>
+<i class="fa fa-user user" ></i>
+</div>
+<div class="box3">
+</div>
+<div class="textp1">
+<p>Looking to establish a career in the field of Information Technology, I wish to work in a</p>
+<div class="textp2">                                                                                                                                                                                                                                                                                                                          
+<P>growth-oriented company and contribute to the development of the organization while </p>
+</div>
+<div class="textp3">
+<P>upgrading my skill set and knowledge.</P>
+</div>
+</div>
+
+<!--------------Projects----------------->
+
+<div class="projects">
+<p id="proj">PROJECTS</p>
+<i class="fa fa-book book"></i>
+</div>
+<div class="box4">
+</div>
+<div class="ocms">
+<ul>
+<li class="firstProject">Online Crime Management System</li>
+<li class="secondProject">Water Monitoring System</li>
+</ul>
+</div>
+
+<!-------------------training projects------------->
+<div class="TProjects">
+<p id="tproj">TRAININGS AND PROJECTS</p>
+<i class="fa fa-tasks tasks"></i>
+</div>
+<div class="box5">
+</div>
+<div class="summer">
+<ul>
+<li class="TrainingPython">Summer Training in Python with IoT from Ducat, College of Engineering Roorkee.</li>
+<li class="PythonProject">Antitheft Device project using Python with Iot.</li>
+<li class="TrainingJava">Training in Java from CDAC, Jaipur.</li>
+</ul>
+
+</div>
+<!----------------------Skills-------------------------->
+<div class="skills">
+<i class= "fa fa-cogs skillsIcon"></i>
+<p id="skill">SKILLS</p>
+</div>
+<div class="box6">
+<div class="row">
+<div class="c">
+<p>C</p>
+</div>
+<div class="python">
+<p>Python</p>
+</div>
+<div class="photoshop">
+<p>Photoshop</p>
+</div>
+<div class="indesign">
+<p>Indesign</p>
+</div>
+<div class="java">
+<p>Java</p>
+</div>
+<div class="html">
+<p>HTML</p>
+</div>
+<div class="css">
+<p>CSS</p>
+</div>
+</div>
+</div>
+<!---------------------Personal Skills------------------->
+<div class="personal">
+    <i class= "fa fa-user-plus user-plus"></i>
+    <p id="personalskills">PERSONAL SKILLS</p>
+    </div>
+    <div class="box7">
+    </div>
+    <div class="skilllist">
+        <ul>
+            <li class="PS1">Positive Attitude, Ambiticious to learn.</li>
+            <li class="PS2">Having Creativity and Imagination.</li>
+            <li class="PS3">Adaptable friendly & open-minded.</li>
+            <li class="PS4">Patience, Responsible and Hardworking.</li>
+        
+        </ul>
+    </div>
+
+<!--------------Extracurricular Activities--------------->
+<div class="Extracurricular">
+    <i class= "fa fa-users users"></i>
+    <p id="ExtracurricularActivities">EXTRACURRICULAR ACTIVITIES</p>
+    </div>
+    <div class="box8">
+    </div>
+    <div class="Activitieslist">
+        <ul>
+            <li class="A1">Worked as a member in Abhyudaya, The Literary Committee of College of Engineering Roorkee.</li>
+            <li class="A2">Worked as member in NGO "PANKHURI".</li>
+            <li class="A3">Worked as member in NGO "PRAHARSH".</li>
+        </ul>
+    </div>
+<!----------------------Declaration--------------------------->
+<div class="Declaration">
+    <p class="dec">I hereby declare that above mentioned Information is correct upto best of my knowledge and I bear the responsibility<br>
+    for the correctness of the above mentioned particulars.</p>
+</div>
+
+
+</body>
+</html>
